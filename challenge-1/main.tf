ef8888860b8e9dd55bc7f23b80da25fb1757e9e5
Omkar Sathe <omkarsathe.pers@gmail.com>
2024-11-06 12:22:32 +0530

    Add NodePort service

--- challenge-1/main.tf
+++ challenge-1/main.tf
@@ -2,7 +2,7 @@ resource "kubernetes_deployment" "frontend" {
   metadata {
     name = "frontend"
     labels = {
-      name  = "simple-webapp"
+      name = "frontend"
     }
   }
 
@@ -33,5 +33,24 @@ resource "kubernetes_deployment" "frontend" {
         }
       }
     }
+  }
+}
+
+resource "kubernetes_service" "webapp-service" {
+  metadata {
+    name = "webapp-service"
+  }
+  spec {
+    selector = {
+      name = kubernetes_deployment.frontend.metadata.0.labels.name
+    }
+
+    port {
+      node_port   = 30080
+      port        = 8080
+      target_port = 8080
+    }
+
+    type = "NodePort"
   }
 }
